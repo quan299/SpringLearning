54e1d4e1460b7b82268baedb6e71346e24e94af2
tung <nguyentungquan200@gmail.com>
2026-07-13 01:58:54 -0400

    new

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,10 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.request.StudentCreationRequest;
 import com.example.demo.dto.request.StudentUpdateRequest;
+import com.example.demo.dto.response.ApiResponse;
 import com.example.demo.dto.response.StudentResponse;
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
-import org.springframework.stereotype.Controller;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,34 +23,74 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<StudentResponse> getAllStudents() {
-        return studentService.getAllStudents();
+
+    public ApiResponse<List<StudentResponse>> getAllStudents() {
+        return new ApiResponse<>(
+                1000,
+                "Success",
+                studentService.getAllStudents()
+        );
     }
 
     @PostMapping
-    public StudentResponse createStudent(@RequestBody StudentCreationRequest request) {
-        return studentService.createStudent(request);
+    public ResponseEntity<ApiResponse<StudentResponse>> createStudent(
+            @Valid @RequestBody StudentCreationRequest request
+    ) {
+        StudentResponse student = studentService.createStudent(request);
+
+        ApiResponse<StudentResponse> response = new ApiResponse<>(
+                1000,
+                "Success",
+                student
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 
     @GetMapping("/{id}")
-    public StudentResponse getStudentById(@PathVariable Long id) {
-        return studentService.getStudentById(id);
+    public ApiResponse<StudentResponse> getStudentById(@PathVariable Long id) {
+        return new ApiResponse<>(
+                1000,
+                "Success",
+                studentService.getStudentById(id)
+        );
     }
 
     @PutMapping("/{id}")
-    public StudentResponse updateStudentById(
+    public ApiResponse<StudentResponse> updateStudentById(
             @PathVariable Long id,
-            @RequestBody StudentUpdateRequest request
+            @Valid @RequestBody StudentUpdateRequest request
     ) {
-        return studentService.updateStudentById(id, request);
+        return new ApiResponse<>(
+                1000,
+                "Success",
+                studentService.updateStudentById(id, request)
+        );
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudentById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deleteStudentById(@PathVariable Long id) {
         studentService.deleteStudentById(id);
+
+        ApiResponse<Void> response = new ApiResponse<>(
+                1000,
+                "Success",
+                null
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
+
     @GetMapping("/search")
-    public StudentResponse findStudentByName(@RequestParam String name) {
-        return studentService.findStudentByName(name);
+    public ApiResponse<StudentResponse> findStudentByName(@RequestParam String name) {
+        return new ApiResponse<>(
+                1000,
+                "Success",
+                studentService.findStudentByName(name)
+        );
     }
 }
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.exception;
 
+import com.example.demo.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException exception){
+    public ResponseEntity<ApiResponse<Void>> handleRuntimeException(RuntimeException exception){
+
+        ApiResponse<Void> response = new ApiResponse<>();
+
+        response.setCode(1001);
+        response.setMessage(exception.getMessage());
+        response.setResult(null);
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+                .body(response);
 
     }
 
